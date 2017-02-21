37141ae4e65ace4364439fb184f7b83dd5bc3e38
Александр <>
2017-02-21 19:40:59 +0200

    Начало верстки блока b-platform

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	 heightItemModule.action();
+	// console.log("Hello");
+
+});
+
+
+var heightItemModule = (function(){
+	var	list  = $('.b-features__list'),
+		itemClass = '.b-features__item',
+		itemsArray = list.children(itemClass);
+
+
+
+	return {
+		action : function(){
+
+		}
+	}
+})();
